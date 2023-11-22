e8a8ce9736ffd6151545370f62883362fe5be56d
Jeremy Singh <jeremys1199@gmail.com>
2023-11-22 17:38:53 -0500

    Ship moves

--- Asteroids.java
+++ Asteroids.java
@@ -7,11 +7,13 @@ NOTE: This class is the metaphorical "main method" of your program,
 import java.awt.*;
 import java.awt.event.*;
 
-class Asteroids extends Game {
+class Asteroids extends Game implements KeyListener{
   private Ship ship;  // Declare the Ship instance
 
   public Asteroids() {
     super("Asteroids!",800,600);
+
+    this.addKeyListener(this);  // Listen for key presses
 
     Point[] shipPoints = {new Point(0, -10), new Point(-10, 10), new Point(10, 10)};  // Define the ship's shape
     Point shipPosition = new Point(width / 2, height / 2);  // Set the ship's position
@@ -19,14 +21,51 @@ class Asteroids extends Game {
   }
   
 	public void paint(Graphics brush) {
+    System.out.println("Asteroids.paint()");
     brush.setColor(Color.black);  //Color of the background
     brush.fillRect(0,0,width,height); //Color from top left to bottom right
 
-    ship.paint(brush);  // Draw the ship
-    ship.move();  // Move the ship
+    if (ship != null){
+      ship.paint(brush);  // Draw the ship
+    }
   }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Key pressed: " + e.getKeyCode());
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            // Move the ship to the right
+            ship.move(10, 0);
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            // Move the ship to the left
+            ship.move(-10, 0);
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            // Move the ship up
+            ship.move(0, -10);
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            // Move the ship down
+            ship.move(0, 10);
+        }
+        // Schedule a repaint
+        repaint();
+    }
+
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_LEFT) {
+            // Stop the ship's horizontal movement when the right or left arrow key is released
+            ship.stopHorizontalMovement();
+        } else if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_DOWN) {
+            // Stop the ship's vertical movement when the up or down arrow key is released
+            ship.stopVerticalMovement();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Leave this method blank
+    }
   
 	public static void main (String[] args) {
-    new Asteroids();
+    Asteroids game = new Asteroids();
   }
 }
--- Ship.java
+++ Ship.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 
 class Ship extends Polygon {
+    private int dx = 0;  // The ship's horizontal speed
+    private int dy = 0;  // The ship's vertical speed
 
     public Ship(Point[] inShape, Point inPosition, double inRotation) {
         // Call the superclass constructor to create the polygon
@@ -26,12 +28,35 @@ class Ship extends Polygon {
         // Draw the polygon
         brush.drawPolygon(xPoints, yPoints, points.length);
     }
-    public void move() {
-    position.x += 1;
+    
+    public void move(int dx, int dy) {
+        System.out.println("Ship position: " + position.x + ", " + position.y);
+        position.x += dx;
+        // If the ship goes off the screen on the right, make it appear on the left
+        if (position.x > 800) {
+            position.x = 0;
+        }
+        // If the ship goes off the screen on the left, make it appear on the right
+        else if (position.x < 0) {
+            position.x = 800;
+        }
 
-    // If the ship goes off the screen, make it appear on the other side
-    if (position.x > 800) {
-        position.x = 0;
+        position.y += dy;
+        // If the ship goes off the screen at the top, make it appear at the bottom
+        if (position.y > 600) {
+            position.y = 0;
+        }
+        // If the ship goes off the screen at the bottom, make it appear at the top
+        else if (position.y < 0) {
+            position.y = 600;
+        }
     }
+
+    public void stopHorizontalMovement() {
+        dx = 0;
+    }
+
+    public void stopVerticalMovement() {
+        dy = 0;
     }
 }
